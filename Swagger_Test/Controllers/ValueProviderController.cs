9882e3e9d7cc6d9f21fd5e862093356511f764cb
HelderSepu <HelderSepu@gmail.com>
2017-05-31 09:35:45 -0400

    Add ValueProviderController

---
+++ Swagger_Test/Controllers/ValueProviderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.ValueProviders;
+
+namespace Swagger_Test.Controllers
+{
+    public class ValueProviderController : ApiController
+    {
+        public IHttpActionResult Get([ValueProvider(typeof(MyValueProviderFactory))] long id)
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        public void Post([FromBody]string value)
+        {
+        }
+
+        private class MyValueProviderFactory: ValueProviderFactory
+        {
+            public MyValueProviderFactory() { }
+
+            public override IValueProvider GetValueProvider(HttpActionContext actionContext)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
