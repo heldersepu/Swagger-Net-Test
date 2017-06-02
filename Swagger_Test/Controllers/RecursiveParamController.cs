e134a615eb8afd04ad28a4a51bf52196e9ea13d2
HelderSepu <HelderSepu@gmail.com>
2017-06-02 11:08:07 -0400

    Add RecursiveParamController

---
+++ Swagger_Test/Controllers/RecursiveParamController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        public CategoryModel Parent { get; set; }
+    }
+
+    public class RecursiveParamController : ApiController
+    {
+        public int Post(CategoryModel value)
+        {
+            return 1;
+        }
+    }
+}
