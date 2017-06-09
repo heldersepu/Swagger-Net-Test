30aac2e92bab7887ca39ef0d757a84ea02a923c1
HelderSepu <HelderSepu@gmail.com>
2017-06-09 16:00:40 -0400

    Testing RoutePrefix

---
+++ Swagger_Test/Controllers/RoutesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("faux/RoutePrefix")]
+    public class RoutesController : ApiController
+    {
+        public string Get()
+        {
+            return "RoutePrefix_Get";
+        }
+
+        [Route("{id:int}")]
+        public string GetById(int id)
+        {
+            return "RoutePrefix_GetById";
+        }
+    }
+}
