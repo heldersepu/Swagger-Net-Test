8d30f1c7b60557be4202a2ce33271f0518e9f69b
HelderSepu <HelderSepu@gmail.com>
2017-06-15 14:29:30 -0400

    Add a MemoryBarrier test controller

---
+++ Swagger_Test/Controllers/MemoryBarrierController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class MemoryBarrierController : ApiController
+    {
+        // GET: api/MemoryBarrier
+        public bool Get()
+        {
+            bool ret = false;
+#if HAVE_MEMORY_BARRIER
+            ret = true;
+#endif
+            return ret;
+        }
+    }
+}
