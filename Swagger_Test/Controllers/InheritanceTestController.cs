911bf68e0cf6af3ee5d8278e6dd988eda8c4dc8d
HelderSepu <HelderSepu@gmail.com>
2017-06-23 21:01:25 -0400

    Add InheritanceTestController

---
+++ Swagger_Test/Controllers/InheritanceTestController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public abstract class Feature
+    {
+        /// <summary>We all need a Title</summary>
+        public string Title { get; set; }
+    }
+
+    public class ImageFeature : Feature
+    {
+        /// <summary>Here goes your image URL</summary>
+        public string ImageUrl { get; set; }
+    }
+
+    public class InheritanceTestController : ApiController
+    {
+        public ImageFeature Get([FromUri]ImageFeature imageFeature)
+        {
+            return imageFeature;
+        }
+
+        public ImageFeature Post(ImageFeature imageFeature)
+        {
+            return imageFeature;
+        }
+    }
+}
