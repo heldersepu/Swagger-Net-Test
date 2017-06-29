6ddc07f3d36388f231e49a92c45c96808f15e94e
HelderSepu <HelderSepu@gmail.com>
2017-06-29 08:30:35 -0400

    EnableCors

--- Swagger_Test/App_Start/WebApiConfig.cs
+++ Swagger_Test/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Swagger_Test
 {
@@ -10,6 +11,7 @@ namespace Swagger_Test
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             config.Formatters.Clear();
             config.Formatters.Add(new BrowserJsonFormatter());
             // Web API routes
