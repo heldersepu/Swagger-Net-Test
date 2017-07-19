eb89ab65a3e735f022104c153f2f7af21bc8bf48
HelderSepu <HelderSepu@gmail.com>
2017-07-19 09:56:10 -0400

    Add SwaggerResponse sample to the Post

--- Swagger_Test/Controllers/IHttpActionResultController.cs
+++ Swagger_Test/Controllers/IHttpActionResultController.cs
@@ -31,9 +31,21 @@ namespace Swagger_Test.Controllers
                 return BadRequest("id must be greater than 0");
         }
 
-        public IHttpActionResult Post()
+        [SwaggerResponse(HttpStatusCode.OK, "List of customers", typeof(IEnumerable<int>))]
+        [SwaggerResponse(HttpStatusCode.NotFound, Type = typeof(NotFoundResult))]
+        public IHttpActionResult Post(MyData data)
         {
             throw new NotImplementedException();
         }
+    }
+
+    /// <summary>My super duper data</summary>
+    public class MyData
+    {
+        /// <summary>The unique identifier</summary>
+        public int id { get; set; }
+
+        /// <summary>Everyone needs a name</summary>
+        public string name { get; set; }
     }
 }
