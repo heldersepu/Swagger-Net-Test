24383837c007648ba229940dd186059e77ac1279
HelderSepu <HelderSepu@gmail.com>
2017-07-21 10:07:59 -0400

    Add Image controller.

---
+++ Swagger_Test/Controllers/ImageController.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class ImageController : ApiController
+    {
+        // GET: api/Image
+        public HttpResponseMessage Get()
+        {
+            string filePath = @"file.png";
+            if (!File.Exists(filePath))
+            {
+                Bitmap bmp = new Bitmap(250, 50);
+                for (int y = 0; y < bmp.Height; ++y)
+                    for (int x = 0; x < bmp.Width; ++x)
+                        bmp.SetPixel(x, y, (x % 5 == 0)? Color.Red : Color.Cyan);
+                bmp.Save(filePath);
+            }
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StreamContent(new FileStream(filePath, FileMode.Open));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            return response;
+        }
+    }
+}
+
