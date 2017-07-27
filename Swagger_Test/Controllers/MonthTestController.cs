1421c6d98b59f9100d678d3ba709bd4d177065b2
HelderSepu <heldersepu@gmail.com>
2017-07-27 16:55:46 -0400

    Add MonthEnum and a sample controller

---
+++ Swagger_Test/Controllers/MonthTestController.cs
@@ -0,0 +1,28 @@
+﻿using Swagger_Test.Models;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class MonthTestController : ApiController
+    {
+        public MonthEnum Get(MonthEnum month)
+        {
+            return month;
+        }
+
+        public MonthEnum Post(MonthEnum month)
+        {
+            return month;
+        }
+
+        public MonthEnum Put(MonthEnum month)
+        {
+            return month;
+        }
+
+        public MonthEnum Delete(MonthEnum month)
+        {
+            return month;
+        }
+    }
+}
---
+++ Swagger_Test/Models/MonthEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace Swagger_Test.Models
+{
+    public enum MonthEnum
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
