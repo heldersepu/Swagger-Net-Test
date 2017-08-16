fc6f423609c2f31f682ded6778c5583926b8cfa3
HelderSepu <HelderSepu@gmail.com>
2017-08-16 15:26:10 -0400

    Add MultiGetController

---
+++ Swagger_Test/Controllers/MultiGetController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Swagger_Test.Controllers
+{
+    public class MultiGetController : ApiController
+    {
+        [ResponseType(typeof(IEnumerable<int>))]
+        public IHttpActionResult Get(int bId)
+        {
+            return Ok();
+        }
+
+        [ResponseType(typeof(IEnumerable<int>))]
+        public IHttpActionResult GetByList(IList<int> cIds)
+        {
+            return Ok();
+        }
+
+        [ResponseType(typeof(IEnumerable<int>))]
+        public IHttpActionResult GetByIdAndList(int bId, IList<int> cIds)
+        {
+            return Ok();
+        }
+    }
+}
