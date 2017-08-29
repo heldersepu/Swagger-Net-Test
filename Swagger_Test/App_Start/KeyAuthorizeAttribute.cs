86cb2e73350f41912e2c66c3f096dc74648dd5ac
HelderSepu <HelderSepu@gmail.com>
2017-08-29 08:22:11 -0400

    Add KeyAuthorizeAttribute

---
+++ Swagger_Test/App_Start/KeyAuthorizeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Swagger_Test
+{
+    public class KeyAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            IEnumerable<string> apiKey = null;
+            if (actionContext.Request.Headers.TryGetValues("apiKey", out apiKey))
+            {
+                return apiKey.First() == "123456";
+            }
+            return false;
+        }
+    }
+}
