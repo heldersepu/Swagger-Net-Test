356d2b74880e09c51b7df61a7deb0f6694e92584
HelderSepu <HelderSepu@gmail.com>
2017-09-06 09:22:31 -0400

    Test controller with actions that take 2 params

---
+++ Swagger_Test/Controllers/IActionResultController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class IActionResultController : ApiController
+    {
+        // GET: api/IActionResult
+        public string[] GetListStuff(int foo, string auditId)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST: api/IActionResult
+        public string[] PostListStuff(int foo, string auditId)
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
