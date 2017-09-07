789754d20d90d5a5763ebf866153571adfb73e7b
HelderSepu <heldersepu@gmail.com>
2017-09-07 10:18:20 -0400

    Add OAuthController

---
+++ Swagger_Test/Controllers/OAuthController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class OAuthController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var obj = new
+            {
+                HttpContext.Current.User.Identity.AuthenticationType,
+                HttpContext.Current.User.Identity.IsAuthenticated,
+                HttpContext.Current.User.Identity.Name
+            };
+            return Ok(obj);
+        }
+    }
+}
