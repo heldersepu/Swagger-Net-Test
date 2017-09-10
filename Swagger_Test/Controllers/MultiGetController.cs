50e53cf8b4bc830b9c499922241d675581889e7e
HelderSepu <heldersepu@gmail.com>
2017-09-10 02:19:25 -0400

    Add Routes to the MultiGetController

--- Swagger_Test/Controllers/MultiGetController.cs
+++ Swagger_Test/Controllers/MultiGetController.cs
@@ -4,20 +4,24 @@ using System.Web.Http.Description;
 
 namespace Swagger_Test.Controllers
 {
+    [RoutePrefix("api/MultiGet")]
     public class MultiGetController : ApiController
     {
+        [Route("")]
         [ResponseType(typeof(IEnumerable<int>))]
         public IHttpActionResult Get(int bId)
         {
             return Ok();
         }
 
+        [Route("ByList")]
         [ResponseType(typeof(IEnumerable<int>))]
         public IHttpActionResult GetByList(IList<int> cIds)
         {
             return Ok();
         }
 
+        [Route("ByIdAndList")]
         [ResponseType(typeof(IEnumerable<int>))]
         public IHttpActionResult GetByIdAndList(int bId, IList<int> cIds)
         {
