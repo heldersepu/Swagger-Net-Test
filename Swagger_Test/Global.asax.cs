2ae21e43deace4882f2a58198c85109b8f625996
HelderSepu <heldersepu@gmail.com>
2017-09-17 20:18:43 -0400

    Change the ContractResolver

--- Swagger_Test/Global.asax.cs
+++ Swagger_Test/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
 
 namespace Swagger_Test
 {
@@ -7,6 +8,7 @@ namespace Swagger_Test
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new DefaultContractResolver();
         }
     }
 }
