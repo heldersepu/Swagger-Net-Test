7817595a15c3e88999cc0e525ee69a9ac2b2ea10
HelderSepu <HelderSepu@gmail.com>
2017-09-20 21:13:52 -0400

    Add BlobController

---
+++ Swagger_Test/Controllers/BlobController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("Blob")]
+    public class BlobController : ApiController
+    {
+        /// <summary>
+        /// Get a Bad Blob
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="includes"></param>
+        /// <returns></returns>
+        [Route("GetBad")]
+        public string GetBad(Guid id, int? includes = null)
+        {
+            return "Bad";
+        }
+
+        /// <summary>
+        /// Get an Ok Blob
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="includes"></param>
+        /// <returns></returns>
+        [Route("GetOk")]
+        public string GetOk(Guid id, int includes = 0)
+        {
+            return "Ok";
+        }
+    }
+}
