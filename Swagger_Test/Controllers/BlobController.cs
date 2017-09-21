021e523914ae27497a5abda4d4ed8260e0ca69e6
HelderSepu <HelderSepu@gmail.com>
2017-09-21 09:19:41 -0400

    Remove default values

--- Swagger_Test/Controllers/BlobController.cs
+++ Swagger_Test/Controllers/BlobController.cs
@@ -5,13 +5,13 @@ namespace Swagger_Test.Controllers
     public abstract class Blob<T> : ApiController
     {
         /// <summary> Get a Bad Blob </summary>
-        public string GetBad(int? x = 1)
+        public string GetBad(int? x)
         {
             return "Bad";
         }
 
         /// <summary> Get an Ok Blob </summary>
-        public string PostOk(int x = 0)
+        public string PostOk(int x)
         {
             return "Ok";
         }
