fb435add35227df8049ce8d4f8e3d034ccea2b21
HelderSepu <HelderSepu@gmail.com>
2017-09-26 15:21:42 -0400

    Add AsyncController
    Testing Issue #25 async and routes

---
+++ Swagger_Test/Controllers/AsyncController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("Async")]
+    public class AsyncController : ApiController
+    {
+        [Route("")]
+        public async Task<IEnumerable<string>> Get()
+        {
+            await Task.Delay(10);
+            return new string[] { "value1", "value2" };
+        }
+
+        [Route("MyService/{myParameter}")]
+        public async Task<string> Get(int myParameter)
+        {
+            await Task.Delay(10);
+            return "value";
+        }
+
+        [Route("")]
+        public async Task Post([FromBody]string value)
+        {
+            await Task.Delay(10);
+        }
+
+        [Route("{id}")]
+        public async Task Put(int id, [FromBody]string value)
+        {
+            await Task.Delay(10);
+        }
+
+        [Route("{id}")]
+        public async Task Delete(int id)
+        {
+            await Task.Delay(10);
+        }
+    }
+}
