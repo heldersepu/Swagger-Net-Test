71e65df330908513bc51aab3e567ed69d8daec32
HelderSepu <heldersepu@gmail.com>
2017-10-10 20:38:48 -0400

    Add NestedEnum Controller

---
+++ Swagger_Test/Controllers/NestedEnumController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public abstract class NestedEnum : ApiController
+    {
+        public enum Giorno { Lunedi, Martedi, Venerdi }
+
+
+        /// <summary> A nullable enum test </summary>
+        public string PutEnumNull(Giorno value)
+        {
+            return "Nullable Enum parameter";
+        }
+
+        /// <summary> A non-null enum test </summary>
+        public string DeleteEnumNonNull(Giorno value)
+        {
+            return "Non-Null Enum parameter";
+        }
+    }
+
+    public class NestedEnumController : NestedEnum { }
+}
