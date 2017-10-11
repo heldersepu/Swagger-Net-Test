27987bed2cada864c00f4c687150830119531f0f
HelderSepu <HelderSepu@gmail.com>
2017-10-11 08:55:45 -0400

    Add action with multiple FromUri

--- Swagger_Test/Controllers/FromUriController.cs
+++ Swagger_Test/Controllers/FromUriController.cs
@@ -6,6 +6,12 @@ namespace Swagger_Test.Controllers
     [RoutePrefix("api/FromUri")]
     public class FromUriController : ApiController
     {
+        [Route("Test0_WithFromUri")]
+        public ViewModelTest GetWith0([FromUri] ViewModelTest v1, [FromUri] ViewModelTest v2)
+        {
+            return v1;
+        }
+
         [Route("Test1_WithFromUri")]
         public ViewModelTest GetWith([FromUri] ViewModelTest v)
         {
