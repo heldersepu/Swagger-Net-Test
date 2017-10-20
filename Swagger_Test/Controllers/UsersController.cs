f12b3b746b9690d9699c49d552e7529d9f52e3ad
HelderSepu <HelderSepu@gmail.com>
2017-10-20 11:23:33 -0400

    Add UsersController

---
+++ Swagger_Test/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public IEnumerable<string> Post(User[] users)
+        {
+            return users.Select(x => x.Name);
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
