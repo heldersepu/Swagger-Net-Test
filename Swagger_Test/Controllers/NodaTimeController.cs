34c27fa59d454863c228775661bd2f5c411bb106
HelderSepu <HelderSepu@gmail.com>
2017-10-24 11:19:29 -0400

    Testing issue with NodaTime as param

---
+++ Swagger_Test/Controllers/NodaTimeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Http;
+using NodaTime.Extensions;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("NodaTime")]
+    public class NodaTimeController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(NodaTime.NodaConstants.BclEpoch.ToString());
+        }
+
+        [Route("my/route1/{date}")]
+        public IHttpActionResult GetByDate1(NodaTime.LocalDate date)
+        {
+            return Ok(date.ToString());
+        }
+
+        [Route("my/route2/{date}")]
+        public IHttpActionResult GetByDate2([FromUri] NodaTime.LocalDate date)
+        {
+            return Ok(date.ToString());
+        }
+
+        [Route("my/route3/{date}")]
+        public IHttpActionResult GetByDate3(DateTime date)
+        {
+            try {
+                var ndate = date.ToLocalDateTime().Date;
+                return Ok(date.ToString());
+            } catch  {
+                return BadRequest("Date is not a valid NodaTime.LocalDate");
+            }            
+        }
+    }
+}
