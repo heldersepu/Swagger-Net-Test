9a87411bdebbe22ba42c03cff08b13440b7650d1
HelderSepu <heldersepu@gmail.com>
2017-10-26 20:13:02 -0400

    Add ChildApiController

---
+++ Swagger_Test/Controllers/ChildApiController.cs
@@ -0,0 +1,28 @@
+﻿using Swagger.Net.Annotations;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    abstract public class BaseApiController : ApiController
+    {
+        [HttpGet]
+        [Route("/api/data")]
+        [SwaggerOperation("GetData")]
+        [SwaggerResponse(200, type: typeof(List<Data>))]
+        public virtual IHttpActionResult GetData()
+        {
+            string exampleJson = null;
+
+            return Ok(exampleJson);
+        }
+    }
+
+    public class ChildApiController : BaseApiController
+    {
+        public override IHttpActionResult GetData()
+        {
+            return base.GetData();
+        }
+    }
+}
