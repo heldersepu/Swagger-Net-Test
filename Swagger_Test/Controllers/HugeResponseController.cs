f975afc1ccf3d68bd85dc9fb55d6770ae3f1c5f9
HelderSepu <HelderSepu@gmail.com>
2017-10-28 17:03:32 -0400

    Add a HugeResponseController

---
+++ Swagger_Test/Controllers/HugeResponseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class HugeResponseController : ApiController
+    {
+        // GET: api/HugeResponse
+        public IEnumerable<string> Get(int max = 10000)
+        {
+            var huge = new List<string>();
+            huge.Add("Testing large responses");
+            for (int i = 0; i < max; i++)
+            {
+                huge.Add(i.ToString());
+                huge.Add(DateTime.Now.Ticks.ToString());
+            }
+            return huge;
+        }
+    }
+}
