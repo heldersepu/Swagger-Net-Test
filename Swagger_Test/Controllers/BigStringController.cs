a1fd4129b320523b5060b19d87d9a5a493ee2b43
HelderSepu <HelderSepu@gmail.com>
2017-10-28 22:10:35 -0400

    Add BigStringController

---
+++ Swagger_Test/Controllers/BigStringController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class BigStringController : ApiController
+    {
+        // GET: api/BigString/5
+        public string Get(int max = 10000)
+        {
+            var big = new StringBuilder();
+            for (int i = 0; i < max; i++)
+            {
+                big.Append(" ");
+                big.Append(i.ToString());
+                big.Append(",");
+                big.Append(DateTime.Now.Ticks.ToString());
+            }
+            return big.ToString();
+        }
+    }
+}
