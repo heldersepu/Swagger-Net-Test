5de699da13b0ec9f017ec1301e69a21828ba1f33
HelderSepu <HelderSepu@gmail.com>
2017-11-03 12:07:44 -0400

    Add  class MyPagedList : IPagedList<Company>

--- Swagger_Test/Controllers/PagedListController.cs
+++ Swagger_Test/Controllers/PagedListController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
 using X.PagedList;
+using System.Collections;
 
 namespace Swagger_Test.Controllers
 {
@@ -21,7 +22,12 @@ namespace Swagger_Test.Controllers
             return Ok(PagedCompany);
         }
 
-        private IPagedList<Company> PagedCompany
+        public MyPagedList Post()
+        {
+            return null;
+        }
+
+        private PagedList<Company> PagedCompany
         {
             get
             {
@@ -30,6 +36,60 @@ namespace Swagger_Test.Controllers
                     data.Add(new Company { Id = i, Name = i.ToString() });
                 return new PagedList<Company>(data, 1, 3);
             }
+        }
+    }
+
+    public class MyPagedList : IPagedList<Company>
+    {
+        public int Abc { get; }
+        public int Def { get; }
+
+        public Company this[int index]
+        {
+            get
+            {                
+                return new Company { Id = 1, Name = "Acme" };
+            }
+        }
+
+        public int Count { get; }        
+
+        public int PageCount { get; }
+
+        public int TotalItemCount { get; }
+
+        public int PageNumber { get; }
+
+        public int PageSize { get; }
+
+        public bool HasPreviousPage { get; }
+
+        public bool HasNextPage { get; }
+
+        public bool IsFirstPage { get; }
+
+        public bool IsLastPage { get; }
+
+        public int FirstItemOnPage { get; }
+
+        public int LastItemOnPage { get; }
+        
+        public IEnumerator<Company> GetEnumerator()
+        {
+            var data = new List<Company>();
+            for (int i = 0; i < 10; i++)
+                data.Add(new Company { Id = i, Name = i.ToString() });
+            return null;
+        }
+
+        public PagedListMetaData GetMetaData()
+        {
+            return this.GetMetaData();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 }
