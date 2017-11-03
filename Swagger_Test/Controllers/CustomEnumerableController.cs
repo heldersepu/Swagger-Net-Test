32e2af0140d6bdd72b948d541e33342e35b5e273
HelderSepu <HelderSepu@gmail.com>
2017-11-03 12:50:45 -0400

    Add CustomEnumerableController

---
+++ Swagger_Test/Controllers/CustomEnumerableController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class CustomEnumerableController : ApiController
+    {
+        // GET: api/CustomEnumerable
+        public MyEnumerable Get()
+        {
+            return null;
+        }
+    }
+
+    public class MyEnumerable : IEnumerable
+    {
+        public int Abc { get; }
+        public int Def { get; }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
