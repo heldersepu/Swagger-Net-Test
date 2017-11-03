1704201e81a7ece7590f3db3a46887419bed31b5
HelderSepu <HelderSepu@gmail.com>
2017-11-03 09:44:07 -0400

    simplify PagedListController

--- Swagger_Test/Controllers/PagedListController.cs
+++ Swagger_Test/Controllers/PagedListController.cs
@@ -25,19 +25,9 @@ namespace Swagger_Test.Controllers
         {
             get
             {
-                var data = new List<Company>
-                {
-                    new Company
-                    {
-                        Id = 12345,
-                        Name = "Acme Co",
-                        Description = "My sample company"
-                    }
-                };
+                var data = new List<Company>();
                 for (int i = 0; i < 10; i++)
-                {
                     data.Add(new Company { Id = i, Name = i.ToString() });
-                }
                 return new PagedList<Company>(data, 1, 3);
             }
         }
