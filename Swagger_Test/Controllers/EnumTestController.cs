2b0bcadb8ff78a34ee40a61715581cd667ca3e68
HelderSepu <heldersepu@gmail.com>
2017-11-09 18:43:42 -0500

    Add EnumTestController

---
+++ Swagger_Test/Controllers/EnumTestController.cs
@@ -0,0 +1,14 @@
+﻿using Swagger_Test.Models;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class EnumTestController : ApiController
+    {
+        public string Get(ShiftDayOffRule rule)
+        {
+            return "test";
+        }
+
+    }
+}
