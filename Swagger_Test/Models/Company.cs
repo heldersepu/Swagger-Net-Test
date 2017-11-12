e67e5c479d4edfe4f559e0d87c26142028b9f167
HelderSepu <heldersepu@gmail.com>
2017-11-12 18:08:17 -0500

    Add a few properties to the Company & examples

--- Swagger_Test/Models/Company.cs
+++ Swagger_Test/Models/Company.cs
@@ -2,8 +2,23 @@
 {
     public class Company
     {
+        /// <example>123</example>
         public int Id { get; set; }
+
+        /// <example>Acme co</example>
         public string Name { get; set; }
+
+        /// <example>Super duper company</example>
         public string Description { get; set; }
+
+        /// <example>42rty</example>
+        public string TaxCode { get; set; }
+
+        /// <example>Tax Code Display</example>
+        public string TaxCodeDisplayName { get; set; }
+
+        public string lowercase { get; set; }
+        public string UPPERCASE { get; set; }
+        public string huNGariAnCASE { get; set; }
     }
 }
