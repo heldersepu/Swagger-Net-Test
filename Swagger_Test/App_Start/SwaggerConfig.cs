6f8f3497843591e0017dc0a6d1b0f207082f1efb
HelderSepu <HelderSepu@gmail.com>
2017-11-17 14:51:00 -0500

    Enable ResolveConflictingActions

--- Swagger_Test/App_Start/SwaggerConfig.cs
+++ Swagger_Test/App_Start/SwaggerConfig.cs
@@ -206,7 +206,7 @@ namespace Swagger_Test
                     // with the same path (sans query string) and HTTP method. You can workaround this by providing a
                     // custom strategy to pick a winner or merge the descriptions for the purposes of the Swagger docs
                     //
-                    //c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 
                     // Wrap the default SwaggerGenerator with additional behavior (e.g. caching) or provide an
                     // alternative implementation for ISwaggerProvider with the CustomProvider option.
