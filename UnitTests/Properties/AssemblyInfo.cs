34f74838bb7fce6c816c9dd6d99be7689d7da8c7
HelderSepu <HelderSepu@gmail.com>
2017-11-17 14:27:18 -0500

    Add a UnitTest that generates a swagger.json

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autonation")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Autonation 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ead150f4-5350-4376-aeb0-49ac40043fe5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/SwaggerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using Swagger.Net.Application;
+using Swagger_Test;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using System.Web.Http.Routing;
+
+namespace UnitTests
+{
+    [TestClass()]
+    public class SwaggerTests
+    {
+        private SwaggerDocsHandler Handler(Action<SwaggerDocsConfig> configure = null)
+        {
+            var swaggerDocsConfig = new SwaggerDocsConfig();
+            configure?.Invoke(swaggerDocsConfig);
+            return new SwaggerDocsHandler(swaggerDocsConfig);
+        }
+
+        private HttpRequestMessage Request
+        {
+            get
+            {
+                var Configuration = new HttpConfiguration();
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://tempuri.org/swagger/docs/v1");
+                request.Properties[HttpPropertyKeys.HttpConfigurationKey] = Configuration;
+
+                string routeTemplate = "swagger/docs/{apiVersion}";
+                var route = new HttpRoute(routeTemplate);
+                var routeData = route.GetRouteData("/", request) ?? new HttpRouteData(route);
+
+                request.Properties[HttpPropertyKeys.HttpRouteDataKey] = routeData;
+                return request;
+            }
+        }
+         
+        [TestMethod()]
+        public void GenerateSwaggerJson()
+        {           
+            var handler = Handler(SwaggerConfig.SwaggerDocConf);
+            var responseMessage =  new HttpMessageInvoker(handler)
+                .SendAsync(Request, new CancellationToken(false)).Result;
+            var responseContent = responseMessage.Content.ReadAsAsync<JObject>().Result;
+            Assert.IsNotNull(responseContent);
+            File.WriteAllText("swagger.json", responseContent.ToString());
+        }
+    }
+}
