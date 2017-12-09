d8fc725db9f614d74ea533afe78e0d474f69a531
HelderSepu <HelderSepu@gmail.com>
2017-12-09 13:39:44 -0500

    Add a ComplexRequestController

---
+++ Swagger_Test/Controllers/ComplexRequestController.cs
@@ -0,0 +1,13 @@
+﻿using Swagger_Test.Models;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class ComplexRequestController : ApiController
+    {
+        public EntityRequest Get(EntityRequest obj)
+        {
+            return obj;
+        }
+    }
+}
---
+++ Swagger_Test/Models/ComplexModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Swagger_Test.Models
+{
+    public class EntityRequest
+    {
+        public Entity person { get; set; }
+    }
+
+    public class Entity
+    {
+        public string Description { get; set; }
+        public EntityName entityName { get; set; }
+    }
+
+    public class EntityName
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
