3566ec4349ad43922c675e2a786d007ed6bccb8b
Helder <heldersepu@gmail.com>
2017-12-31 03:58:51 -0500

    Add PolygonVolumeController

---
+++ Swagger_Test/Controllers/PolygonVolumeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class PolygonVolumeController : ApiController
+    {
+        public PolygonVolumeInsideParameter Get(PolygonVolumeInsideParameter p)
+        {
+            return p;
+        }
+
+        public PolygonVolumeInsideParameter Post(PolygonVolumeInsideParameter p)
+        {
+            return p;
+        }
+    }
+
+    public class PolygonVolumeInsideParameter
+    {
+        public List<Location> Points { get; set; }
+        public string PlanId { get; set; }
+    }
+    public class Location
+    {
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
+    }
+}
