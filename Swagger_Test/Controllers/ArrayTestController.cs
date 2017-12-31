7e5a1042256f45dd2f0445769944926e82f121a4
Helder <heldersepu@gmail.com>
2017-12-31 04:52:32 -0500

    Add ArrayTestController

---
+++ Swagger_Test/Controllers/ArrayTestController.cs
@@ -0,0 +1,29 @@
+﻿using Swagger_Test.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("api/ArrayTest")]
+    public class ArrayTestController : ApiController
+    {
+        [Route("list_string")]
+        public List<string> Get1([FromUri] List<string> p)
+        {
+            return p;
+        }
+
+        [Route("list_int")]
+        public List<int> Get2([FromUri] List<int> p)
+        {
+            return p;
+        }
+
+        [Route("list_location")]
+        public List<Location> Get3([FromUri] List<Location> p)
+        {
+            return p;
+        }
+
+    }
+}
