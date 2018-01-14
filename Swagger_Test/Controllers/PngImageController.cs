554af7f594c67adffecbee6370709ee78790fa07
HelderSepu <HelderSepu@gmail.com>
2018-01-14 06:15:36 -0500

    Add UPCA action

--- Swagger_Test/Controllers/PngImageController.cs
+++ Swagger_Test/Controllers/PngImageController.cs
@@ -1,6 +1,10 @@
-﻿using System.Drawing;
+﻿using Swagger.Net.Annotations;
+using System;
+using System.Drawing;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web.Http;
 
 namespace Swagger_Test.Controllers
 {
@@ -13,6 +17,30 @@ namespace Swagger_Test.Controllers
             var response = new HttpResponseMessage();
             response.Content = ImageStream(Color.Red, Color.Blue);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            return response;
+        }
+
+        [HttpPost, Route("api/upc-a")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, "Empty UPC code.")]
+        [SwaggerResponse(HttpStatusCode.ExpectationFailed, "Invalid UPC code.")]
+        [SwaggerResponse(HttpStatusCode.OK, "Image in image/png format", mediaType: "image/png")]
+        public HttpResponseMessage UPCA([FromBody]string value)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception("Invalid UPC code.");
+            }
+            else if(value.Length < 10)
+            {
+                response.StatusCode = HttpStatusCode.ExpectationFailed;
+                response.Content = new StringContent("Invalid UPC code.");
+            }
+            else
+            {
+                response.Content = ImageStream(RandomColor, RandomColor);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+            }
             return response;
         }
     }
