4d51938f67201c19e176b2b4170cf8b6d1303030
HelderSepu <HelderSepu@gmail.com>
2018-01-14 08:21:27 -0500

    Change max to 250

--- Swagger_Test/Controllers/TestPostController.cs
+++ Swagger_Test/Controllers/TestPostController.cs
@@ -6,7 +6,7 @@ namespace Swagger_Test.Controllers
 {
     public class MyTest
     {
-        [MaxLength(200)]
+        [MaxLength(250)]
         [DefaultValue("HelloWorld")]
         public string Name { get; set; }
         public bool IsPassing { get; set; }
