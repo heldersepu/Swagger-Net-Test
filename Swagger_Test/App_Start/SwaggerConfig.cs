38a31e0ee700faf91cc38d005ae1c5f4bec3e1f3
HelderSepu <HelderSepu@gmail.com>
2018-01-16 14:42:28 -0500

    Testing a controller with param in the Route

--- Swagger_Test/App_Start/SwaggerConfig.cs
+++ Swagger_Test/App_Start/SwaggerConfig.cs
@@ -190,6 +190,7 @@ namespace Swagger_Test
                     c.DocumentFilter<YamlDocumentFilter>();
                     c.DocumentFilter<TestDocumentFilter>();
                     c.DocumentFilter<DocumentFilterAddFakes>();
+                    c.DocumentFilter<RouteTestDocumentFilter>();
                     c.DocumentFilter<SortModelDocumentFilter>();
                     c.DocumentFilter<StringEnumDocumentFilter>();
                     c.DocumentFilter<AddExampleDocumentFilter>();
@@ -482,6 +483,30 @@ namespace Swagger_Test
                     if (del != null)
                     {
                         del.parameters[0].schema.example = Guids;
+                    }
+                }
+            }
+        }
+
+        private class RouteTestDocumentFilter : IDocumentFilter
+        {
+            const string PATH = "/api/RouteTest/test/{itemid}";
+
+            public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry s, IApiExplorer a)
+            {
+                if (swaggerDoc.paths != null && swaggerDoc.paths.ContainsKey(PATH))
+                {
+                    var get = swaggerDoc.paths[PATH].get;
+                    if (get != null)
+                    {
+                        get.parameters.RemoveAt(0);
+                        get.parameters[0].@in = "path";
+                        foreach (var param in get.parameters)
+                        {
+                            int pos = param.name.IndexOf('.');
+                            if (pos > 0)
+                                param.name = param.name.Substring(pos + 1);
+                        }
                     }
                 }
             }
---
+++ Swagger_Test/Controllers/RouteTestController.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    /// <summary>
+    /// Testing a controller with param in the Route
+    /// </summary>
+    [RoutePrefix("api/RouteTest")]
+    public class RouteTestController : ApiController
+    {
+        [Route("test/{itemid:int}")]
+        public DTOv2 Get([FromUri] DTOv2 data)
+        {
+            return data;
+        }
+    }
+
+    public class DTOv2
+    {
+        /// <summary>Secret ID for your object</summary>
+        /// <example>234</example>
+        [DefaultValue(123)]
+        public int itemid { get; set; }
+
+
+        /// <summary>Notes about your object</summary>
+        /// <example>MyNote</example>
+        [DefaultValue("HelloWorld")]
+        public string note { get; set; }
+    }
+}
