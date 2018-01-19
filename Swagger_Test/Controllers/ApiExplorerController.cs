d20a2d37983b56afde40db92f24fe545c76babaf
HelderSepu <HelderSepu@gmail.com>
2018-01-19 11:46:43 -0500

    Testing <remarks> tag

--- Swagger_Test/Controllers/ApiExplorerController.cs
+++ Swagger_Test/Controllers/ApiExplorerController.cs
@@ -4,9 +4,27 @@ using System.Web.Http;
 
 namespace Swagger_Test.Controllers
 {
+
     public class ApiExplorerController : ApiController
     {
-        // GET: api/ApiExplorer
+        /// <summary>
+        /// Testing the summary on the ApiExplorerController
+        /// </summary>
+        /// <remarks>
+        /// Testing the description on the ApiExplorerController
+        /// This is a second line
+        /// Here is a link to <a href="https://github.com/heldersepu">my GitHub profile</a>
+        /// <br/>
+        /// Some HTML styling: <b>BOLD</b> <i>italics</i>
+        /// <ul>
+        /// <li>Item one</li>
+        /// <li>Item two</li>
+        /// </ul>
+        /// <br/>
+        /// <h1>Header1</h1>
+        /// <h2>Header2</h2>
+        /// <h3>Header3</h3>
+        /// </remarks>
         public IEnumerable<string> Get()
         {
             var apiEx = GlobalConfiguration.Configuration.Services.GetApiExplorer();
