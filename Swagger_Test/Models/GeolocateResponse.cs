a36923a25055b8a4cddbca0c3f0e0aa2714cc54d
Helder Sepulveda <heldersepu@gmail.com>
2018-01-28 12:41:21 -0500

    Add examples to the GeolocateResponse

--- Swagger_Test/Models/GeolocateResponse.cs
+++ Swagger_Test/Models/GeolocateResponse.cs
@@ -18,21 +18,25 @@
         /// <summary>
         /// City of IP address
         /// </summary>
+        /// <example>Miami</example>
         public string City;
 
         /// <summary>
         /// State/region code of IP address
         /// </summary>
+        /// <example>FL</example>
         public string RegionCode;
 
         /// <summary>
         /// State/region of IP address
         /// </summary>
+        /// <example>Florida</example>
         public string RegionName;
 
         /// <summary>
         /// Zip or postal code of IP address
         /// </summary>
+        /// <example>33323</example>
         public string ZipCode;
 
         /// <summary>
@@ -43,11 +47,13 @@
         /// <summary>
         /// Latitude of IP address
         /// </summary>
+        /// <example>25.85</example>
         public decimal Latitude;
 
         /// <summary>
         /// Longitude of IP address
         /// </summary>
+        /// <example>-80.27</example>
         public decimal Longitude;
     }
 }
