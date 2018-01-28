03a9c48c748c51b1879fb9ddf8d9d392ba69dd71
Helder Sepulveda <heldersepu@gmail.com>
2018-01-28 09:24:36 -0500

    Add GeolocateResponse to project

--- Swagger_Test/Models/GeolocateResponse.cs
+++ Swagger_Test/Models/GeolocateResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Swagger_Test.Models
+﻿namespace Swagger_Test.Models
 {
     /// <summary>
     /// Geolocation result
