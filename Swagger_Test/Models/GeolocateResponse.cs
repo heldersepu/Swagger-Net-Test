8ecb9c50a1f5ab7f7dfdae30acc5f71edfc1d01c
Helder Sepulveda <heldersepu@gmail.com>
2018-01-28 09:24:03 -0500

    Add GeolocateResponse model
    Model with description in a mix of properties AND fields

---
+++ Swagger_Test/Models/GeolocateResponse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swagger_Test.Models
+{
+    /// <summary>
+    /// Geolocation result
+    /// </summary>
+    public class GeolocateResponse
+    {
+        /// <summary>
+        /// Two-letter country code of IP address
+        /// </summary>
+        public string CountryCode { get; set; }
+
+        /// <summary>
+        /// Country name of IP address
+        /// </summary>
+        public string CountryName;
+
+        /// <summary>
+        /// City of IP address
+        /// </summary>
+        public string City;
+
+        /// <summary>
+        /// State/region code of IP address
+        /// </summary>
+        public string RegionCode;
+
+        /// <summary>
+        /// State/region of IP address
+        /// </summary>
+        public string RegionName;
+
+        /// <summary>
+        /// Zip or postal code of IP address
+        /// </summary>
+        public string ZipCode;
+
+        /// <summary>
+        /// Timezone of IP address
+        /// </summary>
+        public string TimezoneStandardName;
+
+        /// <summary>
+        /// Latitude of IP address
+        /// </summary>
+        public decimal Latitude;
+
+        /// <summary>
+        /// Longitude of IP address
+        /// </summary>
+        public decimal Longitude;
+    }
+}
