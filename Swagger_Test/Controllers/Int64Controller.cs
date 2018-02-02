de2cd0172f9d4d1f5ce9b8e0d984e8a99b970130
HelderSepu <HelderSepu@gmail.com>
2018-02-02 11:17:00 -0500

    Testing with System.Int64

--- Swagger_Test/Controllers/Int64Controller.cs
+++ Swagger_Test/Controllers/Int64Controller.cs
@@ -10,15 +10,25 @@ namespace Swagger_Test.Controllers
             return new Int64Data(1253261109769874438);
         }
 
+        public Int64Data Post(Int64Data data)
+        {
+            return data;
+        }
+
         public class Int64Data
         {
             /// <example>1253261109769874438</example>
-            public long MyInt64 { get; set; }
+            public long MyLong { get; set; }
+
+            /// <example>21474836470</example>
+            public System.Int64 MyInt64 { get; set; }
+
             /// <example>1253261109769874438</example>
             public string MyData { get; set; }
 
             public Int64Data(long int64)
             {
+                MyLong = int64;
                 MyInt64 = int64;
                 MyData = int64.ToString();
             }
