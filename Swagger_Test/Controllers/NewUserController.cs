5155092f6e94b8255e3977578a0c172b85e86198
HelderSepu <HelderSepu@gmail.com>
2018-02-19 12:16:15 -0500

    Add NewUserController

---
+++ Swagger_Test/Controllers/NewUserController.cs
@@ -0,0 +1,13 @@
+﻿using Swagger_Test.Models;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class NewUserController : ApiController
+    {
+        public NewUserWriteModel Get([FromUri] NewUserWriteModel data)
+        {
+            return data;
+        }
+    }
+}
