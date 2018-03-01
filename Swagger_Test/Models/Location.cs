04eb852441d47f485373a9603c570cb9e74ff4c7
HelderSepu <HelderSepu@gmail.com>
2018-03-01 10:55:24 -0500

    Add proper examples

--- Swagger_Test/Models/Location.cs
+++ Swagger_Test/Models/Location.cs
@@ -4,9 +4,11 @@ namespace Swagger_Test.Models
 {
     public class Location
     {
+        /// <example>25.85</example>
         [Range(-90, 90)]
         public decimal Lat { get; set; }
 
+        /// <example>-80.27</example>
         [Range(-180, 180)]
         public decimal Lon { get; set; }
     }
