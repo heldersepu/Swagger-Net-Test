3e808edde96fc9bc046fb0ca5693dc2bd0d2ce22
HelderSepu <HelderSepu@gmail.com>
2018-03-01 13:45:56 -0500

    Add HtmlExampleController

---
+++ Swagger_Test/Controllers/HtmlExampleController.cs
@@ -0,0 +1,37 @@
+﻿using Swagger.Net.Annotations;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class HtmlExampleController : ApiController
+    {
+        [SwaggerResponse(HttpStatusCode.OK, type: typeof(string), mediaType: "text/html")]
+        public async Task<HttpResponseMessage> Get(Guid id)
+        {
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(Example, Encoding.UTF8, "text/html")
+            };
+        }
+
+        private static string Example
+        {
+            get
+            {
+                return @"
+                    <!DOCTYPE html>
+                    <html>
+                        <head></head>
+                        <body>
+                            <h1>hello</h1>
+                        </body>
+                    </html>";
+            }
+        }
+    }
+}
