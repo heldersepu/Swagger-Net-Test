a71b71dff7b7ade3e56e28d35617a7d9309eb01b
HelderSepu <HelderSepu@gmail.com>
2018-03-01 10:48:53 -0500

    Add Range to the Latitude and longitude

--- Swagger_Test/Models/Location.cs
+++ Swagger_Test/Models/Location.cs
@@ -1,8 +1,13 @@
-﻿namespace Swagger_Test.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Swagger_Test.Models
 {
     public class Location
     {
+        [Range(-90, 90)]
         public decimal Lat { get; set; }
+
+        [Range(-180, 180)]
         public decimal Lon { get; set; }
     }
 }
