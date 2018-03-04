c39482b48b569353f4ad6f5962e9bd309ed2ccc7
Helder Sepulveda <heldersepu@gmail.com>
2018-03-04 09:57:02 -0500

    Add LocationController

---
+++ Swagger_Test/Controllers/LocationController.cs
@@ -0,0 +1,21 @@
+﻿using Swagger_Test.Models;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("Location")]
+    public class LocationController : ApiController
+    {
+        [Route("Get1")]
+        public GeolocateResponse Get1([FromUri] GeolocateResponse x)
+        {
+            return x;
+        }
+
+        [Route("Get2")]
+        public Location Get2([FromUri] Location x)
+        {
+            return x;
+        }
+    }
+}
