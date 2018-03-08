10ab9776f05c8f86304f984b8d45fdc9e1996509
Helder Sepulveda <heldersepu@gmail.com>
2018-03-08 08:39:32 -0500

    Testing long descriptions

--- Swagger_Test/Models/Location.cs
+++ Swagger_Test/Models/Location.cs
@@ -9,12 +9,12 @@ namespace Swagger_Test.Models
     /// </summary>
     public class Location
     {
-        /// <summary>Latitude</summary>
+        /// <summary>**Latitude**: a geographic coordinate that specifies the north–south position of a point on the Earth's surface.</summary>
         /// <example>25.85</example>
         [Range(-90, 90)]
         public float Lat { get; set; }
 
-        /// <summary>Longitude</summary>
+        /// <summary>**Longitude**: a geographic coordinate that specifies the east-west position of a point on the Earth's surface.</summary>
         /// <example>-80.27</example>
         [Range(-180, 180)]
         public float Lon { get; set; }
