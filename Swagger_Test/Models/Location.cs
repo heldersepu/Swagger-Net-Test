5843bd9b4bfd26a1fc4828b327ab08a07fddb63d
Helder Sepulveda <heldersepu@gmail.com>
2018-03-08 08:21:28 -0500

    Add summary to the Location class

--- Swagger_Test/Models/Location.cs
+++ Swagger_Test/Models/Location.cs
@@ -2,12 +2,15 @@
 
 namespace Swagger_Test.Models
 {
+    /// <summary>Geographic coordinates</summary>
     public class Location
     {
+        /// <summary>Latitude</summary>
         /// <example>25.85</example>
         [Range(-90, 90)]
         public float Lat { get; set; }
 
+        /// <summary>Longitude</summary>
         /// <example>-80.27</example>
         [Range(-180, 180)]
         public float Lon { get; set; }
