97f5f7121960dae5eb5693d3f9588f9341c1997f
HelderSepu <HelderSepu@gmail.com>
2018-03-09 15:52:18 -0500

    Add TestMultiGetController

---
+++ Swagger_Test/Controllers/TestMultiGetController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class TestMultiGetController : ApiController
+    {
+        public string Get()
+        {
+            return "Get1";
+        }
+
+        public string Get(int id)
+        {
+            return "Get2";
+        }
+    }
+}
