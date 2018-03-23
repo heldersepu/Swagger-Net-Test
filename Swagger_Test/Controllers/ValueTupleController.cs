27991d0520123f5b2b7c24f7ff118c3cb75598e9
HelderSepu <HelderSepu@gmail.com>
2018-03-23 10:55:06 -0400

    Add test for ValueTuple

---
+++ Swagger_Test/Controllers/ValueTupleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class ValueTupleController : ApiController
+    {
+        // GET: api/ValueTuple
+        public IHttpActionResult Get()
+        {
+            var unit = (10, 20, 'X');
+            Tuple<int, int, char> tuple = unit.ToTuple();
+            return Ok(tuple);
+        }
+    }
+}
