a7941277b3a5a2ff6defa0b5981f7adeabbaa4c9
HelderSepu <HelderSepu@gmail.com>
2018-04-09 14:54:44 -0400

    Break down the long line

--- Swagger_Test/Controllers/BigSwagController.cs
+++ Swagger_Test/Controllers/BigSwagController.cs
@@ -10,7 +10,9 @@ namespace Swagger_Test.Controllers
         public IHttpActionResult Get(int id)
         {
             var swag = new StringBuilder();
-            swag.Append("{\"swagger\":\"2.0\",\"info\":{\"version\":\"V1\",\"title\":\"Test\"},\"host\":\"swagger-net-test.azurewebsites.net\",\"schemes\":[\"http\"],\"paths\":{\"/api/test\":{\"get\":{\"operationId\":\"test_Get\",\"consumes\":[],\"produces\":[\"application/json\"],\"parameters\":[],\"responses\":{\"200\":{\"description\":\"OK\",\"schema\":{\"$ref\":\"#/definitions/Test1K\"}}}}}},\"definitions\":{\"Location\":{\"properties\":{\"Lat\":{\"type\":\"number\",\"format\":\"float\"},\"Lon\":{\"type\":\"number\",\"format\":\"float\"}},\"xml\":{\"name\":\"Location\"}},\"Test1K\":{\"properties\":{");
+            swag.Append("{\"swagger\":\"2.0\",\"info\":{\"version\":\"V1\",\"title\":\"Test\"},\"host\":\"swagger-net-test.azurewebsites.net\",\"schemes\":[\"http\"],");
+            swag.Append("\"paths\":{\"/api/test\":{\"get\":{\"operationId\":\"test_Get\",\"consumes\":[],\"produces\":[\"application/json\"],\"parameters\":[],\"responses\":{\"200\":{\"description\":\"OK\",\"schema\":{\"$ref\":\"#/definitions/Test1K\"}}}}}},");
+            swag.Append("\"definitions\":{\"Location\":{\"properties\":{\"Lat\":{\"type\":\"number\",\"format\":\"float\"},\"Lon\":{\"type\":\"number\",\"format\":\"float\"}},\"xml\":{\"name\":\"Location\"}},\"Test1K\":{\"properties\":{");
             for (int i = 0; i < id; i++)
                 swag.Append("\"val"+ i.ToString() + "\":{\"$ref\":\"#/definitions/Location\"},");
             swag.Append("\"val\":{\"$ref\":\"#/definitions/Location\"}");
