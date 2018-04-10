54c5d26247716d506d9773db7d90fa13a14b9805
HelderSepu <HelderSepu@gmail.com>
2018-04-10 11:39:11 -0400

    Add MultiParamPostController

---
+++ Swagger_Test/Controllers/MultiParamPostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("MultiParamPost")]
+    public class MultiParamPostController : ApiController
+    {
+        [Route("{id}")]
+        public Company Post(Guid id, List<Company> companies)
+        {
+            return companies.FirstOrDefault();
+        }
+    }
+}
