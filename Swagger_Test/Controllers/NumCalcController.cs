c230fe55304c79e33da82961d5f882594bd63b91
HelderSepu <HelderSepu@gmail.com>
2018-04-17 13:22:52 -0400

    Add NumCalcController

---
+++ Swagger_Test/Controllers/NumCalcController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class NumCalcController : ApiController
+    {
+        public double Get(long max = 1000000)
+        {
+            double total = 0;
+            for (long i = 1; i < max; i++)
+                total += 1/(double)i;
+            return total;
+        }
+    }
+}
