f55f9df9336b669072dec13152ebcd0ee380e9b5
Helder Sepulveda <heldersepu@gmail.com>
2018-04-18 09:43:27 -0400

    Testing FromBodyController

---
+++ Swagger_Test/Controllers/FromBodyController.cs
@@ -0,0 +1,32 @@
+﻿using Swagger_Test.Models;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("api/FromBody")]
+    public class FromBodyController : ApiController
+    {
+        [Route("Test0_WithFromBody")]
+        public IHttpActionResult GetWith0([FromBody] ViewModelTest a, [FromBody] ViewModelTest b)
+        {
+            return Ok(new { a = a, b = b });
+        }
+
+        [Route("Test1_WithFromBody")]
+        public ViewModelTest GetWith([FromBody] ViewModelTest v)
+        {
+            return v;
+        }
+
+        [Route("Test2_NoFromBody")]
+        public ViewModelTest GetWithout(ViewModelTest v)
+        {
+            return v;
+        }
+
+        public ViewModelTest Post(ViewModelTest v)
+        {
+            return v;
+        }
+    }
+}
