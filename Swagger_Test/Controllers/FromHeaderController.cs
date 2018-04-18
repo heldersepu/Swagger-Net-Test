5e244d9b801e7304a587bc4d6dfc1e4d168b4d3d
Helder Sepulveda <heldersepu@gmail.com>
2018-04-18 15:07:57 -0400

    Add FromHeaderController

---
+++ Swagger_Test/Controllers/FromHeaderController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("api/FromHeader")]
+    public class FromHeaderController : ApiController
+    {
+        [Route("Test0_WithFromUri")]
+        public IHttpActionResult GetWith0([FromHeader] string a, [FromHeader] string b)
+        {
+            return Ok(new { a = a, b = b });
+        }
+
+        [Route("Test1_WithFromHeader")]
+        public string GetWith([FromHeader] string v)
+        {
+            return v;
+        }
+
+        [Route("Test2_NoFromHeader")]
+        public string GetWithout(string v)
+        {
+            return v;
+        }
+
+        public string Post([FromHeader] string v)
+        {
+            return v;
+        }
+    }
+}
