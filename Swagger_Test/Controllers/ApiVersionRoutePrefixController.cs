4678ae8766e8ab2c283c68b0c6cfa5542d8159c0
HelderSepu <HelderSepu@gmail.com>
2018-04-20 11:08:35 -0400

    Add ApiVersionRoutePrefixController

---
+++ Swagger_Test/Controllers/ApiVersionRoutePrefixController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("v1_0/test1")]
+    public class ApiVersionRoutePrefix1Controller : ApiController
+    {
+        [Route("{id:int}")]
+        public string Get(int id)
+        {
+            return "ApiVersionRoutePrefix1_Get";
+        }
+    }
+
+    [RoutePrefix("v2_0/test2")]
+    public class ApiVersionRoutePrefix2Controller : ApiController
+    {
+        [Route("{id:int}")]
+        public string Get(int id)
+        {
+            return "ApiVersionRoutePrefix1_Get";
+        }
+    }
+}
