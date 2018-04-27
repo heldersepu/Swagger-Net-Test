1541502c5b8c2d35e140fe87755fd28c8952fa92
HelderSepu <HelderSepu@gmail.com>
2018-04-27 09:00:39 -0400

    Add LongProcessingController

---
+++ Swagger_Test/Controllers/LongProcessingController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class LongProcessingController : ApiController
+    {
+        /// <summary>
+        /// This action will put the thread to sleep for the given amount of seconds.
+        /// </summary>
+        /// <param name="seconds">How many seconds to sleep</param>
+        /// <returns>The provided seconds</returns>
+        public int Get(int seconds = 25)
+        {
+            Thread.Sleep(seconds * 1000);
+            return seconds;
+        }
+    }
+}
