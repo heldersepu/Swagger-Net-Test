c81e0825a59bb4444728ee6f7454222b14b9473a
HelderSepu <HelderSepu@gmail.com>
2018-04-30 14:51:11 -0400

    Add ResourcesController

---
+++ Swagger_Test/Controllers/ResourcesController.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class ResourcesController : ApiController
+    {
+        public string Get()
+        {
+            foreach (var name in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+            {
+                if (name.EndsWith("js"))
+                {
+                    var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
+                    return new StreamReader(stream).ReadToEnd();
+                }
+            }
+            return null;
+        }
+    }
+}
