e3d58d76741f56a45ec35250de71b93e54dcea30
Helder Sepulveda <heldersepu@gmail.com>
2018-05-02 13:53:45 -0400

    Add ProcessController

---
+++ Swagger_Test/Controllers/ProcessController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class ProcessController : ApiController
+    {
+        // GET: api/Process
+        public IHttpActionResult Get(string name = null)
+        {
+            var data = Process.GetProcesses().Select(x => new {
+                x.Id, x.ProcessName
+            });
+            if (!string.IsNullOrEmpty(name))
+                data = data.Where(x => x.ProcessName.Contains(name));
+            return Ok(data);
+        }
+    }
+}
