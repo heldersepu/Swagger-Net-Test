e69261436c6dc83d5aafb747d3ddf3c00b9b26c9
HelderSepu <HelderSepu@gmail.com>
2018-05-03 14:56:54 -0400

    Add NullablesController

---
+++ Swagger_Test/Controllers/NullablesController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class NullablesController : ApiController
+    {
+        public List<TestUser> Get()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    Name = "Foo",
+                    Email = "foo@bar.com",
+                    UserRole = UserRole.Admin,
+                    Address = null
+                }
+            };
+        }
+
+        public class TestUser
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public UserRole UserRole { get; set; }
+            public string Address { get; set; }
+        }
+
+        public enum UserRole
+        {
+            Admin,
+            Moderator,
+            Member
+        }
+    }
+}
