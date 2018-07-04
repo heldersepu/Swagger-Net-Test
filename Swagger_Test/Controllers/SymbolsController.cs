c9b554fde26367418c84fcd3682d308ae1b40d11
Helder Sepulveda <heldersepu@gmail.com>
2018-07-03 22:12:51 -0400

    Add SymbolsController

---
+++ Swagger_Test/Controllers/SymbolsController.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class SymbolsController : ApiController
+    {
+        public Symbols Post(Symbols x)
+        {
+            return x;
+        }
+    }
+
+    public class Symbols
+    {
+        [JsonProperty(PropertyName = "@id")]
+        public int @id;
+        [JsonProperty(PropertyName = "@name")]
+        public string @name;
+        [JsonIgnore]
+        public string details;
+    }
+}
