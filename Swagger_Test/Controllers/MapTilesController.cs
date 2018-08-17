cbaea5723b2c16d8e780606cbdddeeaba509e1a1
Helder Sepulveda <heldersepu@gmail.com>
2018-08-17 13:04:40 -0400

    Add missing usings

--- Swagger_Test/Controllers/MapTilesController.cs
+++ Swagger_Test/Controllers/MapTilesController.cs
@@ -1,5 +1,6 @@
 ﻿using Swagger.Net.Annotations;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -15,7 +16,7 @@ namespace Swagger_Test.Controllers
         public HttpResponseMessage Get(int z, int x, int y)
         {
             var response = new HttpResponseMessage();
-            var color = Color.FromArgb(100 + z*4, 0, 0)
+            var color = Color.FromArgb(100 + z*4, 0, 0);
             response.Content = ImageStream(color, color, 256, 256);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
             return response;
