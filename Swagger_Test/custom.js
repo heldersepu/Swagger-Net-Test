2132f2252aff6b1a4d7657518215c843ab5f3d25
Helder Sepulveda <heldersepu@gmail.com>
2018-10-09 09:22:27 -0400

    Inject version

--- Swagger_Test/custom.js
+++ Swagger_Test/custom.js
@@ -6,5 +6,5 @@ console.log("custom js")
 tb = document.getElementsByClassName("topbar")[0];
 tb_link = tb.getElementsByClassName("link")[0];
 var small = document.createElement("small");
-small.appendChild(document.createTextNode("HelloWorld!"));
+small.appendChild(document.createTextNode(versions.swaggerUi.version));
 tb_link.appendChild(small);
