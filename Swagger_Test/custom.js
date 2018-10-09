65ee4c244b5f44269797c1f8d35826a07b8a4147
Helder Sepulveda <heldersepu@gmail.com>
2018-10-09 09:15:29 -0400

    Correction to custom code

--- Swagger_Test/custom.js
+++ Swagger_Test/custom.js
@@ -3,8 +3,8 @@
 }
 console.log("custom js")
 
-tb = document.getElementsByClassName("topbar")[0]
-tb_link = tb.getElementsByClassName("link")[0]
+tb = document.getElementsByClassName("topbar")[0];
+tb_link = tb.getElementsByClassName("link")[0];
 var small = document.createElement("small");
-small.appendChild(document.createTextNode("HelloWorld!")
-tb_link.appendChild(small)
+small.appendChild(document.createTextNode("HelloWorld!"));
+tb_link.appendChild(small);
