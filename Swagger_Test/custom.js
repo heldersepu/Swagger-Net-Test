0aa1fbea9dbaf7df24505b1fc63f748d137efb6a
Helder Sepulveda <heldersepu@gmail.com>
2018-10-09 08:49:55 -0400

    Inject something into the topbar link

--- Swagger_Test/custom.js
+++ Swagger_Test/custom.js
@@ -2,3 +2,9 @@
     return "Hello World";
 }
 console.log("custom js")
+
+tb = document.getElementsByClassName("topbar")[0]
+tb_link = tb.getElementsByClassName("link")[0]
+var small = document.createElement("small");
+small.appendChild(document.createTextNode("HelloWorld!")
+tb_link.appendChild(small)
