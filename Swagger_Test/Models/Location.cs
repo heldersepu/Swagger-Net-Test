b0746df739c14dabc121c3def43b9f4c94b04141
Helder Sepulveda <heldersepu@gmail.com>
2019-05-14 09:25:59 -0400

    Update Location.cs

--- Swagger_Test/Models/Location.cs
+++ Swagger_Test/Models/Location.cs
@@ -5,7 +5,7 @@ namespace Swagger_Test.Models
     /// <summary>
     /// ## Geographic coordinates
     /// ___
-    /// A **geographic coordinate system** is a coordinate system used in geography that enables every location on Earth
+    /// A **geographic coordinate system** is a coordinate system used in geography that enables every location on Earth to be specified by a set of numbers
     /// </summary>
     public class Location
     {
