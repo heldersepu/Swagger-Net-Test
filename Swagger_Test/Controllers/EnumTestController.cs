765aaa691c4e5385bfb754e2ad049e1d22cbaa7b
Helder Sepulveda <heldersepu@gmail.com>
2019-05-28 19:26:13 -0400

    Add post to EnumTestController

--- Swagger_Test/Controllers/EnumTestController.cs
+++ Swagger_Test/Controllers/EnumTestController.cs
@@ -10,5 +10,27 @@ namespace Swagger_Test.Controllers
             return rule;
         }
 
+        public ShiftDayOffRule Post(TestEnum data)
+        {
+            return data.shift_day_off_rule;
+        }
+
+    }
+
+    public class TestEnum
+    {
+        /// <summary>Everything needs an ID.</summary>
+        public int id;
+
+        /// <summary>
+        /// <ul>
+        /// <li>None = 0,       // DayOffNotRequired</li>
+        /// <li>OffAfter = 1,   // DayOffAfter next is off</li>
+        /// <li>OffBefore = -1, // DayOffBefore previous is off</li>
+        /// <li>InAfter = 3     // DayInAfter next cannot be off</li>
+        /// </ul>
+        /// </summary>
+        public ShiftDayOffRule shift_day_off_rule;
+
     }
 }
