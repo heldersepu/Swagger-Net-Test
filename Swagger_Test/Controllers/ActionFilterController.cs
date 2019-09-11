4206a1d098cca4c3e4bd4938a984f6dedeadf62e
Helder Sepulveda <heldersepu@gmail.com>
2019-09-11 11:40:39 -0400

    Testing ActionFilterAttribute

---
+++ Swagger_Test/Controllers/ActionFilterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace Swagger_Test.Controllers
+{
+
+    public class ActionFilterController : ApiController
+    {
+        [CustomAuthorize()]
+        public string Post(string x)
+        {
+            return x;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class CustomAuthorizeAttribute : ActionFilterAttribute
+    {
+        public int SecurityCheck { get; set; }
+    }
+}
