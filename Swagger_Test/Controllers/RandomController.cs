99d016b4c4ba4cd41f8ef28822eedbc56b5da879
Helder Sepulveda <heldersepu@gmail.com>
2020-03-26 21:02:40 -0400

    Add  RandomController

---
+++ Swagger_Test/Controllers/RandomController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Swagger_Test
+{
+    public class RandomController : ApiController
+    {
+        private static Random rnd = new Random();
+
+        internal Color RandomColor
+        {
+            get
+            {
+                return Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            }
+        }
+
+        [CacheOutput(ClientTimeSpan = 5, ServerTimeSpan = 5)]
+        public Color GetRandomColor()
+        {
+            return RandomColor;
+        }
+
+        [CacheOutput(ClientTimeSpan = 10, ServerTimeSpan = 10)]
+        public int GetRandomNumber(int max)
+        {
+            return rnd.Next(max);
+        }
+    }
+}
